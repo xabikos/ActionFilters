608c30aeb16719ec54b6d57a5634544d4cf6825b
Babis Karypidis <xabikos@hotmail.com>
2014-11-01 12:31:43 +0200

    Add action filter to check if the current user has verified his email for Web api controllers

---
+++ ActionFilters/Controllers/api/ExampleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using ActionFilters.Infrastructure;
+
+namespace ActionFilters.Controllers.api
+{
+    public class ExampleController : ApiController
+    {
+        // GET: api/Example
+        [UserConfirmedWebApiFilterAttribute]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
---
+++ ActionFilters/Infrastructure/UserConfirmedWebApiFilterAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace ActionFilters.Infrastructure
+{
+    /// <summary>
+    /// Action filter attribute can be used in Web Api Controller class or methods to verify the current user has confirmed his email
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class UserConfirmedWebApiFilterAttribute : ActionFilterAttribute 
+    {
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            var userId = actionContext.RequestContext.Principal.Identity.GetUserId();
+            // User is not logged in so redirect him to log in controller action
+            if (string.IsNullOrEmpty(userId))
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.Unauthorized,
+                    "You must be logged in to access this resource");
+                return Task.FromResult(0);
+            }
+
+            var userManager = actionContext.Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            if (!userManager.IsEmailConfirmed(userId))
+            {
+                actionContext.Response = actionContext.Request.CreateErrorResponse(HttpStatusCode.BadRequest,
+                    "You must be verify your email address in order to access this resource");
+                return Task.FromResult(0);
+            }
+
+            return base.OnActionExecutingAsync(actionContext, cancellationToken);
+        }
+
+    }
+}
